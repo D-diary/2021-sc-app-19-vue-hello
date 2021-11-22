f9ae148ee5be1aabc0358407c24c17183c4d5aa4
dlrlsud@naver.com <dlrlsud@naver.com>
2021-11-22 12:53:33 +0900

    1

---
+++ js/02.gallery.js
@@ -0,0 +1,20 @@
+new Vue({
+	el: '#app',
+	data: {
+		imgs: null,
+		src: '',
+		title: ''
+	},
+	async created() { // $(document).ready(), document.load()
+		const { data } = await axios.get('../json/foods.json')
+		this.imgs = data
+		this.src = this.imgs[0].src
+		this.title = this.imgs[0].title
+	},
+	methods: {
+		showImg(e) {
+			this.src = e.target.src
+			this.title = e.target.alt
+		}
+	}
+})
---
+++ js/03.shop.js
@@ -0,0 +1,27 @@
+new Vue({
+	el: '#app',
+	data: {
+		products: [],	// 원본
+		searchProducts: [],	// 검색결과
+		query: ''
+	},
+	async created() {
+		const { data } = await axios.get('../json/foods.json')
+		this.products = data
+		this.searchProducts = data
+	},
+	methods: {
+		onReset(e) {
+			this.query = ''
+			this.searchProducts = this.products
+			document.querySelector('.search-wrap input').focus()
+		},
+		onSearch(e) {
+			this.query = e.target.value
+			this.searchProducts = this.products.filter(v => {
+				return v.title.toLowerCase().includes(this.query.toLowerCase()) || 
+				v.description.toLowerCase().includes(this.query.toLowerCase())
+			})
+		}
+	}
+})
